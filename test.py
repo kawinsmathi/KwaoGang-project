89fb4f0460d0726ac2a9808959e13f05d6afcfbf
kawinsmathi <thege3236@gmail.com>
2015-12-07 21:15:46 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,66 @@
-from tkinter import *
+import csv
+with open('test1.csv',newline='') as csvfile:
+    a = csv.reader(csvfile)
+    lst = []
+    for row in a:
+        lst.append((','.join(row)).split('\t'))
+    #All
+    over_fifteen = lst[0]
+    total_labor_force = lst[1]
+    employed = lst[2]
+    unemployed = lst[3]
+    seasonally_inactive_labor_force = lst[4]
+    person_not_in_labor_force = lst[5]
+    over_fifteen = list(map(int, over_fifteen))
+    total_labor_force = list(map(int, total_labor_force))
+    employed = list(map(int, employed))
+    unemployed = list(map(int, unemployed))
+    seasonally_inactive_labor_force = list(map(int, seasonally_inactive_labor_force))
+    person_not_in_labor_force = list(map(int, person_not_in_labor_force))
+    print(over_fifteen)
+    print(total_labor_force)
+    print(employed)
+    print(unemployed)
+    print(seasonally_inactive_labor_force)
+    print(person_not_in_labor_force)
 
-def main():
-    global entry1
-    root = Tk()
-    label1 = Label(text='entry your name here')
-    label1.pack()
-    label1.place(x=200, y=180)
+    #Male
+    over_fifteen_m = lst[7]
+    total_labor_force_m = lst[8]
+    employed_m = lst[9]
+    unemployed_m = lst[10]
+    seasonally_inactive_labor_force_m = lst[11]
+    person_not_in_labor_force_m = lst[12]
+    over_fifteen_m = list(map(int, over_fifteen_m))
+    total_labor_force_m = list(map(int, total_labor_force_m))
+    employed_m = list(map(int, employed_m))
+    unemployed_m = list(map(int, unemployed_m))
+    seasonally_inactive_labor_force_m = list(map(int, seasonally_inactive_labor_force_m))
+    person_not_in_labor_force_m = list(map(int, person_not_in_labor_force_m))
+    print(over_fifteen_m)
+    print(total_labor_force_m)
+    print(employed_m)
+    print(unemployed_m)
+    print(seasonally_inactive_labor_force_m)
+    print(person_not_in_labor_force_m)
 
-    entry1 = Entry(bd=2)
-    entry1.pack()
-    entry1.place(x=180, y=220)
+    #Female
+    over_fifteen_f = lst[14]
+    total_labor_force_f = lst[15]
+    employed_f = lst[16]
+    unemployed_f = lst[17]
+    seasonally_inactive_labor_force_f = lst[18]
+    person_not_in_labor_force_f = lst[19]
+    over_fifteen_f = list(map(int, over_fifteen_f))
+    total_labor_force_f = list(map(int, total_labor_force_f))
+    employed_f = list(map(int, employed_f))
+    unemployed_f = list(map(int, unemployed_f))
+    seasonally_inactive_labor_force_f = list(map(int, seasonally_inactive_labor_force_f))
+    person_not_in_labor_force_f = list(map(int, person_not_in_labor_force_f))
+    print(over_fifteen_f)
+    print(total_labor_force_f)
+    print(employed_f)
+    print(unemployed_f)
+    print(seasonally_inactive_labor_force_f)
+    print(person_not_in_labor_force_f)
 
-    button1 = Button(text='click', command=hello)
-    button1.pack()
-    button1.place(x=250, y=260)
-
-    root.mainloop()
-
-def hello():
-    label2 = Label(text=('Hello!! '+ entry1.get()))
-    label2.pack()
-
-if __name__ == '__main__':
-    main()
